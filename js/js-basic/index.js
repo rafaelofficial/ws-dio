1c10cf49909ce1fc0f58a58085bd31ceb1f5a453
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-06-17 02:47:13 -0300

    example basic JavaScript

---
+++ js/js-basic/index.js
@@ -0,0 +1,22 @@
+// Example of variable
+
+const PRICE = 2;
+var discount = 0.2;
+
+var total = PRICE - discount;
+
+// Example of function
+function sum(a, b) {
+    return a + b; // 16
+}
+
+sum(4, 12);
+
+// Example object in JavaScript
+const people = {
+    name : 'Rafael',
+    age : 33,
+    location : 'Nova Odessa - SP'
+}
+
+let result = `Name: ${people.name}, Age ${people.age} and Location: ${people.location}`;
