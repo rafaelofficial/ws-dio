6a88afa9b6159e0044ef42d04e71455803693cb4
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-06-27 15:26:02 -0300

    finish TODO project

--- js/to-do-list/assets/js/script.js
+++ js/to-do-list/assets/js/script.js
@@ -1,36 +1,28 @@
 // Selects of DOM
-
 const form = document.getElementById('form');
 const taskInput = document.getElementById('task-input');
-const buttonPush = document.getElementById('push');
+const btnPush = document.getElementById('btnPush');
 const listTasks = document.getElementById('list');
 
 // Events listening
-
-buttonPush.addEventListener('click', function (event) {
+btnPush.addEventListener('click', function (event) {
     event.preventDefault();
+        // getter elements and created elements
+        let taskCheckbox = document.createElement('input');
+        let taskLabel = document.createElement('label');
 
-    // getter elements and created elements
-    let taskCheckbox = document.createElement('input');
-    let taskLabel = document.createElement('label');
+        // setter attributes
+        taskCheckbox.setAttribute('id', 'checkbox');
+        taskCheckbox.setAttribute('type', 'checkbox');
 
-    // setter attributes
-    taskCheckbox.setAttribute('id', 'checkbox');
-    taskCheckbox.setAttribute('type', 'checkbox');
+        // inners HTML values of input
+        taskLabel.innerHTML = taskInput.value + '<br />'; 
 
-    // inners HTML values of input
-    taskLabel.innerHTML = taskInput.value + '<br />'; 
+        // appended child of input and label
+        listTasks.appendChild(taskCheckbox);
+        listTasks.appendChild(taskLabel);
 
-    // appended child of input and label
-    listTasks.appendChild(taskCheckbox);
-    listTasks.appendChild(taskLabel);
-
-    // reset form
-    form.reset();
+        // reset form
+        form.reset();        
 });
 
-// functions
-
-function clearTasks() {
-
-}
