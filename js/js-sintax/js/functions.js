af036f639ccd88839f5094b137dd1f66ebb905f4
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-06-30 15:05:28 -0300

    demo of functions

---
+++ js/js-sintax/js/functions.js
@@ -0,0 +1,11 @@
+function demoFunctionWithoutParams() {
+    console.log('This is a function without parameter');
+}
+
+demoFunctionWithoutParams();
+
+function demoFunctionWithParams(message) {
+    console.log(message);
+}
+
+demoFunctionWithParams('This is a function with parameter');
