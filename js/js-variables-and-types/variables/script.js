e872d8d463c71289fc3cb582dde774c07e6df88d
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-07-06 12:48:32 -0300

    variables and types

---
+++ js/js-variables-and-types/variables/script.js
@@ -0,0 +1,12 @@
+var firstName = 'João';
+var lastName = 'Sousa';
+
+if (firstName === 'João') {
+    let lastName = 'Rodrigues';
+    var firstName = 'Pedro';
+    lastName = 'Silva';
+
+    console.log(lastName);
+}
+
+console.log(firstName, lastName);
